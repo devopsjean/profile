f361d1b09f2f69c492aa06b38fe11428f382ae1f
devopsjean <devopsjean@users.noreply.github.com>
2026-08-31 10:03:03 +0000

    deploy: 6cf7f7bbaefa3a0dd922a5da7cef60722a9c430b

---
+++ assets/swimlanesDiagram-VR7AAH4N-DPdsGuyd.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-Db8jE3gK.js";import{_ as a}from"./mermaid.core-CR8nyKtt.js";import"./chunk-5VM5RSS4-DdZIySQJ.js";import"./chunk-XXDRQBXY-BnIBJp5m.js";import"./chunk-POPQ4Y6H-DIOmzUU1.js";import"./chunk-F27PBJKO-0bUxfC0w.js";import"./channel-b-zPar1K.js";import"./index-CsEw7xYd.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
